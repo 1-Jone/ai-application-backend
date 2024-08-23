b3cbd4a02f203e286bfde10ce374c42ab6f513cf
GZX <1907916094@qq.com>
2024-08-23 16:12:14 +0800

    第一次提交

---
+++ src/main/java/com/gzx/aidada/service/PostService.java
@@ -0,0 +1,51 @@
+package com.gzx.aidada.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.gzx.aidada.model.dto.post.PostQueryRequest;
+import com.gzx.aidada.model.vo.PostVO;
+import com.gzx.aidada.model.entity.Post;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 帖子服务
+ *
+ */
+public interface PostService extends IService<Post> {
+
+    /**
+     * 校验
+     *
+     * @param post
+     * @param add
+     */
+    void validPost(Post post, boolean add);
+
+    /**
+     * 获取查询条件
+     *
+     * @param postQueryRequest
+     * @return
+     */
+    QueryWrapper<Post> getQueryWrapper(PostQueryRequest postQueryRequest);
+
+    /**
+     * 获取帖子封装
+     *
+     * @param post
+     * @param request
+     * @return
+     */
+    PostVO getPostVO(Post post, HttpServletRequest request);
+
+    /**
+     * 分页获取帖子封装
+     *
+     * @param postPage
+     * @param request
+     * @return
+     */
+    Page<PostVO> getPostVOPage(Page<Post> postPage, HttpServletRequest request);
+}
---
+++ src/test/java/com/gzx/aidada/QuestionControllerTest.java
@@ -0,0 +1,34 @@
+package com.gzx.aidada;
+
+import com.gzx.aidada.model.dto.question.AiGenerateQuestionRequest;
+import com.gzx.aidada.controller.QuestionController;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+@SpringBootTest
+public class QuestionControllerTest {
+
+    @Resource
+    private QuestionController questionController;
+
+    @Test
+    void aiGenerateQuestionSSETest() throws InterruptedException {
+        // 模拟调用
+        AiGenerateQuestionRequest aiGenerateQuestionRequest = new AiGenerateQuestionRequest();
+        aiGenerateQuestionRequest.setAppId(3L);
+        aiGenerateQuestionRequest.setQuestionNumber(10);
+        aiGenerateQuestionRequest.setOptionNumber(2);
+
+        // 模拟普通用户
+        questionController.aiGenerateQuestionSSETest(aiGenerateQuestionRequest, false);
+        // 模拟普通用户
+        questionController.aiGenerateQuestionSSETest(aiGenerateQuestionRequest, false);
+        // 模拟会员用户
+        questionController.aiGenerateQuestionSSETest(aiGenerateQuestionRequest, true);
+
+        // 模拟主线程一直启动
+        Thread.sleep(1000000L);
+    }
+}
